74d976592d557073b9c01a562ba366ceba5801d4
Hidetchi <hidetchi@81dojo.com>
2015-10-18 16:41:44 +0900

    Official Release.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
 
   def updates
+    d = Date.today - 1
+    t = Time.new - 24*60*60
+    @coming_games = Game.includes(:sente, :gote, :tournament).where('result is null and game_date >= ?', d).order(:game_date).limit(8)
+    @coming_events = Event.includes(:country).where('start_time > ?', t).order(:start_time).limit(3)
+    @updated_events = Event.includes(:country).order(updated_at: :desc).limit(3)
+    @new_books = Book.includes(:language).order(issue_date: :desc).limit(3)
+    @new_clubs = Club.includes(:country).order(created_at: :desc).limit(3)
+    @new_websites = Website.includes(:country).order(created_at: :desc).limit(3)
   end
 
   def help
--- app/helpers/countries_helper.rb
+++ app/helpers/countries_helper.rb
@@ -1,4 +1,12 @@
 module CountriesHelper
+  def flag_s_tag(code)
+    image_tag(sprintf("flags_s/%03d.gif", code), :style => 'margin-left:8px;margin-right:8px;vertical-align:text-bottom;')
+  end
+
+  def flag_ss_tag(code)
+    image_tag(sprintf("flags_ss/%03d.png", code), :style => 'margin-left:8px;margin-right:8px;vertical-align:baseline;')
+  end
+
   def flag_80_tag(code)
     image_tag(sprintf("flags_80/%03d.jpg", code))
   end
