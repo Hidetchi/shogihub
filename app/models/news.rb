860a585ef1a86f059194f5765374c7b687fe9e79
Hidetchi_HQ <root@localhost.localdomain>
2016-11-25 21:44:14 -0800

    Support change in JSA official atom.xml

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,14 +2,25 @@ class News < ActiveRecord::Base
   include PublicActivity::Common
 
   def self.fetch_from_JSA
-    url = 'http://www.shogi.or.jp/topics/atom.xml'
+    url = 'http://www.shogi.or.jp/atom.xml'
     feed = Feedjira::Feed.fetch_and_parse url
     feed.entries.reverse.each do |e|
-      unless (News.find_by(entry_id: e.entry_id))
-        category = e.categories =~ /お知らせ/ ? 1 : 2
-        if (news = News.create(entry_id: e.entry_id, url: e.url, published_at: e.published, title_ja: e.title, title_en: e.title, content_ja: e.content, content_en: e.content, category: category))
-          puts news.entry_id.to_s
+      to_load = false
+      e.categories.each do |category|
+        if ["イベント", "メディア情報", "棋戦", "女流棋戦"].include?(category)
+          to_load = false
+          break
+        elsif category == "お知らせ"
+          to_load = true
+        elsif ["棋界ニュース", "昇段・昇級", "キリ勝数"].include?(category)
+          to_load = true
+          break
         end
+      end
+      next unless to_load
+      unless (News.find_by(entry_id: e.entry_id))
+        category = 1
+        News.create(entry_id: e.entry_id, url: e.url, published_at: e.published, title_ja: e.title, title_en: e.title, content_ja: e.content, content_en: e.content, category: category))
       end
     end
   end
